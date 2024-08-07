0f0f0ac4efb931c65f31fe01d0dbe3519a1e3159
Mohamed-Madani <m.rabiouhalilou@gmail.com>
2024-08-07 00:35:38 +0000

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -11,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={cn('')}>
       <body>{children}</body>
     </html>
   )
